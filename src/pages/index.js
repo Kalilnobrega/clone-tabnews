c38af2fde3c57de44d99b67240205fef84f2ab5a
Kalil da Nóbrega Souza <nobregakalilsz@gmail.com>
2025-11-29 02:33:00 +0000

    mudando texto

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,11 @@ function home(){
 
     return (
         <div id="meu-container">
-            <h1>Te amo muito meu amor, se tu tbm me ama, dá uma risadinha</h1>
+            <h1>Um singelo texto de poucas palavras tiradas do meu coração para minha neném. 
+Tu foi a melhor coisa que já me aconteceu, fez minha vida mudar completamente, fez eu ver o mundo com outros olhos, me fez ser a pessoa mais feliz do mundo só por tu ser quem tu é, que é essa pessoa maravilhosa e linda por fora e por dentro.
+Como eu te disse hoje, eu me apaixonei por essa tua pureza, gentileza, bondade e por ser verdadeira, eu nunca imaginei que existia alguém assim, e desde quando eu descobri, prometi que iria te proteger de todo mal.
+Sempre quando eu tô contigo tudo fica melhor e mais divertido que nem a última semana, só eu e tu e mais ninguém, não preciso de mais nada, já tenho tudo.
+Eu coloquei como "singelo texto de poucas palavras" porque não caberia toda a nossa linda história em 1 único texto, mas que eu queria expressar em palavras um pouco do que minha cabeça doida pensa e meu coração sente nesse momento, eu te amo mais que tudo minha vida e minha futura esposa.</h1>
             
             <img src="/gato.jpg" width={300} />
         </div>
