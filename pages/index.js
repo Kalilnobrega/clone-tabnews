ff58ab816b63a8cf1322db7f988bd52114fdaa77
Kalil da Nóbrega Souza <nobregakalilsz@gmail.com>
2025-11-29 02:24:47 +0000

    adiciona imagem

--- pages/index.js
+++
@@ -1,5 +0,0 @@
-function home(){
-    return <h1>Te amo muito meu amor, se tu tbm me ama, dá uma risadinha</h1>
-}
-
-export default home;
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+function home(){
+
+    return (
+        <div id="meu-container">
+            <h1>Te amo muito meu amor, se tu tbm me ama, dá uma risadinha</h1>
+            
+            <img src="/gato.jpg" width={300} />
+        </div>
+
+    );
+}
+export default home;
